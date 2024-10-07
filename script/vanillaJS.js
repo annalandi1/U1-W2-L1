708e9191b46d137735202e041336cf52a8008e88
annalandi1 <landianna.1106@gmail.com>
2024-10-07 14:13:10 +0200

    update vanilla

---
+++ script/vanillaJS.js
@@ -0,0 +1,29 @@
+console.log('hello World!')
+
+console.log("la somma di 10 e 5", 10+5)
+
+//VARIABILI:
+//si chiama variabile perche il suo contenuto puó cambiare nel corso del tempo
+
+//le variabili si definiscono con la parola 'let'
+
+let dog // ho dichiarato una variabile "dog"
+
+let myDog // nomenclatura "camelCase" e di solito on cominciano con dei numeri 
+
+myDog = "Pluto" // ho assegnato alla variabile il vaolore "Pluto"
+
+let myCat = "cat" // la var. viene gia inizzializzata con un contenuto
+
+let age = 25
+
+console.log ("age prima", age)
+
+age = 50 // il nuovo valore di age é 50
+
+console.log ("age dopo", age)
+
+let stringExample = "ciao"
+let numberExample = 145
+let booleanExample = false // booleano, valore true/false
+// un valore booleano serve a dichiarare la veridicità/falsita di un'espressione
